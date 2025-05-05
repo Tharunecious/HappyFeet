82db17d310b076d97db48dea3c5e5e2bbe8e9f7f
Tharunecious <tharunsankar30@gmail.com>
2025-05-05 21:12:31 +0530

    Changes done

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,13 @@ connectCloudinary()
 
 //middlewares
 app.use(express.json())
-app.use(cors())
+app.use(cors());
 
 //api endpoints
-app.use('/api/user',userRouter)
-app.use('/api/product',productRouter)
-app.use('/api/cart',cartRouter)
-app.use('/api/order',orderRouter)
+app.use('api/user',userRouter)
+app.use('api/product',productRouter)
+app.use('api/cart',cartRouter)
+app.use('api/order',orderRouter)
 
 app.get('/', (req,res) => {
     res.send("API Working")
